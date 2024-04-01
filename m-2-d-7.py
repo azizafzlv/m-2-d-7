77a1e8517bf38fcc94fb6a4832cc2f61c867461c
Алимова Азиза <alimovaaziza@Noutbuk-Alimova.local>
2024-04-01 19:49:45 +0600

    second task

--- m-2-d-7.py
+++ m-2-d-7.py
@@ -5,16 +5,30 @@
 lambda функцию тоже, но можно и без нее
 """
 
-cities = ['MOSKOW', 'BISHKEK', 'ALMATY', 'BERLIN', 'ANKARA', 'AMSTERDAM']
+# cities = ['MOSKOW', 'BISHKEK', 'ALMATY', 'BERLIN', 'ANKARA', 'AMSTERDAM']
 
-def make_to_lower(city_name):
-    return city_name.lower()
+# def make_to_lower(city_name):
+#     return city_name.lower()
 
-#первый способ
-cities_lower = list(map(make_to_lower, cities))
+# #первый способ
+# cities_lower = list(map(make_to_lower, cities))
 
-#второй способ
-cities_lower = list(map(lambda city_name: city_name.lower(), cities))
+# #второй способ
+# cities_lower = list(map(lambda city_name: city_name.lower(), cities))
 
-print(cities_lower)
+# print(cities_lower)
+
+#2
+"""
+Отфильтруйте отобрав в новый список только четные числа
+и возведите в третью степень каждую цифру отфильтрованного
+списка
+"""
+
+# numbers = [4, 3, 6, 8, 9, 7, 1, 2, 34, 5, 56, 76]
+
+# numbers = list(filter(lambda x: x % 2 ==0, numbers))
+# numbers = list(map(lambda x: pow(x, 3), numbers))
+
+# print(numbers)
 
